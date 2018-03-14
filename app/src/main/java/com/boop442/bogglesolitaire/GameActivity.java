7d974a8bf939d879815fa7dc1dd4d12f40783d4a
telpuhova <telpuhova@gmail.com>
2018-03-14 15:00:10 -0700

    add making sure mLetters has two vowels

--- app/src/main/java/com/boop442/bogglesolitaire/GameActivity.java
+++ app/src/main/java/com/boop442/bogglesolitaire/GameActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
@@ -18,12 +20,42 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         Random rnd = new Random();
-        char ch;
+        String letter;
+        String[] vowels = new String[] {"a", "o", "e", "i", "u"};
+        List vowelsList = Arrays.asList(vowels);
+
+
+
+        boolean[] itemIsAConsonant = new boolean[8];
+        int vowelCount = 0;
 
         for (int i=0; i<8; i++) {
-            ch = (char) (rnd.nextInt(26) + 'a');
-            mLetters[i] = Character.toString(ch);
+            letter = Character.toString( (char)(rnd.nextInt(26) + 'a') );
+            if (vowelsList.contains(letter)) {
+                vowelCount++;
+            } else {
+                itemIsAConsonant[i] = true;
+            }
+
+            mLetters[i] = letter;
         }
+
+        if (vowelCount < 2) {
+            int j = 0;
+            int i = 0;
+            int k;
+            while (j < (2 - vowelCount)) {
+                if (!vowelsList.contains(mLetters[i])) {
+                    k = (rnd.nextInt(5));
+                    mLetters[i] = vowels[k];
+                    j++;
+                    i++;
+                } else {
+                    i++;
+                }
+            }
+        }
+
 
         gridView = (GridView) findViewById(R.id.baseGridView);
         gridView.setAdapter(new GameAdapter(this, mLetters));
